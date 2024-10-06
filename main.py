b243e65b4cc78961ee2b5f2cd7c98c102f5d140c
jrj37 <jean.raphael2810@icloud.com>
2024-10-06 16:30:11 +0200

    water

--- main.py
+++ main.py
@@ -2,51 +2,8 @@ from settings import *
 from level import *
 from pytmx.util_pygame import load_pygame
 from os.path import join
-
+from game import Game
 from support import *
-
-class Game:
-    def __init__(self):
-        pygame.init()
-        self.display_surface=pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
-        pygame.display.set_caption('SUPER PREGA BROS')
-        self.clock =pygame.time.Clock()
-        self.import_assets()
-        
-        self.tmx_maps={0: load_pygame(join('.','data','levels','omni.tmx'))}
-        self.current_stage=Level(self.tmx_maps[0], self.level_frames)
-
-    def import_assets(self):
-        self.level_frames = {
-            'flag': import_folder('.', 'graphics', 'level', 'flag'),
-            'saw': import_folder('.', 'graphics', 'enemies', 'saw', 'animation'),
-            'floor_spike' : import_folder('.', 'graphics', 'enemies', 'floor_spikes'),
-            'tooth' : import_folder('.','graphics','enemies','tooth','run'),
-            'shell':import_sub_folders('.','graphics','enemies','shell'),
-            'pearl':import_image('.','graphics','enemies','bullets','pearl'),
-            'flag': import_folder('.', 'graphics', 'level', 'flag'),
-            'saw': import_folder('.', 'graphics', 'enemies', 'saw', 'animation'),
-            'floor_spike' : import_folder('.', 'graphics', 'enemies', 'floor_spikes'),
-            'palms' : import_folder('.', 'graphics', 'level', 'palms'),
-            'candle' : import_folder('.', 'graphics', 'level', 'candle'),
-            'window' : import_folder('.', 'graphics', 'level', 'window'),
-            'big_chain' : import_folder('.', 'graphics', 'level', 'big chains'),
-            'small_chain' : import_folder('.', 'graphics', 'level', 'small chains'),
-            'player' : import_sub_folders('.', 'graphics', 'player'),
-            'pearl':import_image('.','graphics','enemies','bullets','pearl'),
-        }
-        
-        
-    def run(self):
-        while True:
-            dt = self.clock.tick()/1000
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-            
-            self.current_stage.run(dt) 
-            pygame.display.update()
 
 if __name__ == "__main__":               
     game=Game()
